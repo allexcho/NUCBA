c489118e4dbbd22a3408d3e9d34adf58db991461
Alex cho <allexcho98@gmail.com>
2024-09-03 11:31:05 -0300

    adding style

--- E3-js/index.js
+++ E3-js/index.js
@@ -1,7 +1,7 @@
 const pizzas = [
   {
     id: 1,
-    nombre: "pizza de Muzzarella",
+    nombre: "Pizza de Muzzarella",
     precio: 500,
     ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
     imagen: "./img/muzzarella.png",
@@ -9,7 +9,7 @@ const pizzas = [
 
   {
     id: 2,
-    nombre: "pizza de Cebolla",
+    nombre: "Pizza de Cebolla",
     precio: 1500,
     ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
     imagen: "./img/cebolla.png",
@@ -17,7 +17,7 @@ const pizzas = [
 
   {
     id: 3,
-    nombre: "pizza 4 Quesos",
+    nombre: "Pizza 4 Quesos",
     precio: 1380,
     ingredientes: [
       "Muzzarella",
@@ -31,7 +31,7 @@ const pizzas = [
 
   {
     id: 4,
-    nombre: "pizza Especial",
+    nombre: "Pizza Especial",
     precio: 1000,
     ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
     imagen: "./img/especial.png",
@@ -39,39 +39,38 @@ const pizzas = [
 
   {
     id: 5,
-    nombre: "pizza con Anana",
+    nombre: "Pizza con Anana",
     precio: 600,
     ingredientes: ["Muzzarella", "Tomate", "Anana"],
     imagen: "./img/anana.png",
   },
 ];
 
+const searchForm = document.getElementById("search-form");
+const pizzaIdInput = document.getElementById("pizza-id");
+const resultContainer = document.getElementById("result-container");
 
-const searchForm = document.getElementById('search-form');
-const pizzaIdInput = document.getElementById('pizza-id');
-const resultContainer = document.getElementById('result-container');
-
-searchForm.addEventListener('submit', (e)=>{
+searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const pizzaId = parseInt(pizzaIdInput.value);
-  searchPizza(pizzaId)
+  searchPizza(pizzaId);
 });
 
-function searchPizza(id){
-  const pizza = pizzas.find(pizza => pizza.id === id);
+function searchPizza(id) {
+  const pizza = pizzas.find((pizza) => pizza.id === id);
 
-  if(!pizzaIdInput.value){
-    renderError('Por favor, ingrese un numero de ID')
-  }else if(!pizza){
-    renderError('No se encontro ninguna pizza con ese ID')
-  }else{
-    renderPizzaCard(pizza)
-    savePizzaToLocalStorage(pizza)
+  if (!pizzaIdInput.value) {
+    renderError("Por favor, ingrese un numero de ID");
+  } else if (!pizza) {
+    renderError("No se encontro ninguna pizza con ese ID");
+  } else {
+    renderPizzaCard(pizza);
+    savePizzaToLocalStorage(pizza);
   }
 }
 
-function renderPizzaCard(pizza){
-    resultContainer.innerHTML = `
+function renderPizzaCard(pizza) {
+  resultContainer.innerHTML = `
     <div class = "pizza-card">
       <h2>${pizza.nombre}</h2>
       <img src="${pizza.imagen}">
@@ -80,19 +79,19 @@ function renderPizzaCard(pizza){
     `;
 }
 
-function renderError(message){
-  resultContainer.innerHTML = `<p class ="error-message">${message}</p`
+function renderError(message) {
+  resultContainer.innerHTML = `<p class ="error-message">${message}</p`;
 }
 
-function savePizzaToLocalStorage(pizza){
-  localStorage.setItem('lastPizza', JSON.stringify(pizza));
+function savePizzaToLocalStorage(pizza) {
+  localStorage.setItem("lastPizza", JSON.stringify(pizza));
 }
 
-function  loadLastPizza(){
-  const lastPizza = localStorage.getItem('lastPizza');
-  if(lastPizza){
+function loadLastPizza() {
+  const lastPizza = localStorage.getItem("lastPizza");
+  if (lastPizza) {
     renderPizzaCard(JSON.parse(lastPizza));
   }
 }
 
-document.addEventListener('DOMContentLoaded', loadLastPizza);
+document.addEventListener("DOMContentLoaded", loadLastPizza);
